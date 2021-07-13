86bf82ed8d0a5c9fa59e671ec26c83a4b64027ae
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2021-07-13 23:34:22 +0800

    [新增] 資料庫建立檢視表，並且匯入EDMX，建立view_Customer

---
+++ MVCHomework_Oen/Controllers/view_CustomerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCHomework_Oen.Models;
+
+namespace MVCHomework_Oen.Controllers
+{
+    public class view_CustomerController : Controller
+    {
+        private CustomerProfileEntities db = new CustomerProfileEntities();
+
+        // GET: view_Customer
+        public ActionResult Index()
+        {
+            return View(db.view_Customer.ToList());
+        }
+
+        // GET: view_Customer/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            view_Customer view_Customer = db.view_Customer.Find(id);
+            if (view_Customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(view_Customer);
+        }
+
+        // GET: view_Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: view_Customer/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "客戶名稱,聯絡人數量,銀行帳戶數量")] view_Customer view_Customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.view_Customer.Add(view_Customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(view_Customer);
+        }
+
+        // GET: view_Customer/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            view_Customer view_Customer = db.view_Customer.Find(id);
+            if (view_Customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(view_Customer);
+        }
+
+        // POST: view_Customer/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "客戶名稱,聯絡人數量,銀行帳戶數量")] view_Customer view_Customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(view_Customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(view_Customer);
+        }
+
+        // GET: view_Customer/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            view_Customer view_Customer = db.view_Customer.Find(id);
+            if (view_Customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(view_Customer);
+        }
+
+        // POST: view_Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            view_Customer view_Customer = db.view_Customer.Find(id);
+            db.view_Customer.Remove(view_Customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- MVCHomework_Oen/Models/CustomerProfile.Context.cs
+++ MVCHomework_Oen/Models/CustomerProfile.Context.cs
@@ -31,6 +31,7 @@ namespace MVCHomework_Oen.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<view_Customer> view_Customer { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
