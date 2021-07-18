6f500e0ea72ef7a296551701488230ba4b5dc506
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2021-07-18 16:35:43 +0800

    將 index.html 加入 /Metro/Index 頁面，並解決路徑問題

---
+++ MVCHomework_Oen/Controllers/MetroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomework_Oen.Controllers
+{
+    public class MetroController : Controller
+    {
+        // GET: Metro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
