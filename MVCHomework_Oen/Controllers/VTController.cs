89f3375dfff6477c26aed672a99a09c41fcfdfb1
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2021-07-18 16:35:03 +0800

    示範 _ViewStart.cshtml 的用途

---
+++ MVCHomework_Oen/Controllers/VTController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomework_Oen.Controllers
+{
+    public class VTController : Controller
+    {
+        // GET: VT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Page1()
+        {
+            return View();
+        }
+    }
+}
